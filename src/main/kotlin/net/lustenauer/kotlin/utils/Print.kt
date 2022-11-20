670b6b4301ebc1d521050433a59251aad65567c4
Patric Hollenstein <patric.hollenstein@lustenauer.net>
2022-11-20 11:33:39 +0100

    Initial commit

---
+++ src/main/kotlin/net/lustenauer/kotlin/utils/Print.kt
@@ -0,0 +1,11 @@
+package net.lustenauer.kotlin.utils
+
+class Print {
+    fun printHelloWorld() {
+        println("Hello World")
+    }
+
+    fun printHalloName(name: String) {
+        println("Hello $name")
+    }
+}
